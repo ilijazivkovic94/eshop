461a4d4735497e6b2577e9ef56d4649b089bf8e5
logichunters <webmaster63@outlook.com>
2023-09-15 18:59:06 +0800

    Code formation of ordercartupdate

--- adm/eyoom_admin/core/shop/orderformcartupdate.php
+++ adm/eyoom_admin/core/shop/orderformcartupdate.php
@@ -11,11 +11,11 @@ auth_check_menu($auth, $sub_menu, "w");
 check_admin_token();
 
 $ct_chk_count = isset($_POST['ct_chk']) ? count($_POST['ct_chk']) : 0;
-if(!$ct_chk_count)
+if (!$ct_chk_count)
     alert('처리할 자료를 하나 이상 선택해 주십시오.');
 
-$status_normal = array('주문','입금','준비','배송','완료');
-$status_cancel = array('취소','반품','품절');
+$status_normal = array('주문', '입금', '준비', '배송', '완료');
+$status_cancel = array('취소', '반품', '품절');
 
 if (in_array($_POST['ct_status'], $status_normal) || in_array($_POST['ct_status'], $status_cancel)) {
     ; // 통과
@@ -32,30 +32,29 @@ $mod_history = '';
 $cnt = (isset($_POST['ct_id']) && is_array($_POST['ct_id'])) ? count($_POST['ct_id']) : 0;
 $arr_it_id = array();
 
-for ($i=0; $i<$cnt; $i++)
-{
-    $k = isset($_POST['ct_chk'][$i]) ? (int) $_POST['ct_chk'][$i] : '';
+for ($i = 0; $i < $cnt; $i++) {
+    $k = isset($_POST['ct_chk'][$i]) ? (int)$_POST['ct_chk'][$i] : '';
 
-    if($k === '') continue;
+    if ($k === '') continue;
 
-    $ct_id = isset($_POST['ct_id'][$k]) ? (int) $_POST['ct_id'][$k] : 0;
+    $ct_id = isset($_POST['ct_id'][$k]) ? (int)$_POST['ct_id'][$k] : 0;
 
-    if(!$ct_id)
+    if (!$ct_id)
         continue;
 
     $sql = " select * from {$g5['g5_shop_cart_table']} where od_id = '$od_id' and ct_id  = '$ct_id' ";
     $ct = sql_fetch($sql);
-    if(! (isset($ct['ct_id']) && $ct['ct_id']))
+    if (!(isset($ct['ct_id']) && $ct['ct_id']))
         continue;
 
     // 수량이 변경됐다면
-    $ct_qty = isset($_POST['ct_qty'][$k]) ? (int) $_POST['ct_qty'][$k] : 0;
-    if($ct['ct_qty'] != $ct_qty) {
+    $ct_qty = isset($_POST['ct_qty'][$k]) ? (int)$_POST['ct_qty'][$k] : 0;
+    if ($ct['ct_qty'] != $ct_qty) {
         $diff_qty = $ct['ct_qty'] - $ct_qty;
 
         // 재고에 차이 반영.
-        if($ct['ct_stock_use']) {
-            if($ct['io_id']) {
+        if ($ct['ct_stock_use']) {
+            if ($ct['io_id']) {
                 $sql = " update {$g5['g5_shop_item_option_table']}
                             set io_stock_qty = io_stock_qty + '$diff_qty'
                             where it_id = '{$ct['it_id']}'
@@ -76,18 +75,16 @@ for ($i=0; $i<$cnt; $i++)
                     where ct_id = '$ct_id'
                       and od_id = '$od_id' ";
         sql_query($sql);
-        $mod_history .= G5_TIME_YMDHIS.' '.$ct['ct_option'].' 수량변경 '.$ct['ct_qty'].' -> '.$ct_qty."\n";
+        $mod_history .= G5_TIME_YMDHIS . ' ' . $ct['ct_option'] . ' 수량변경 ' . $ct['ct_qty'] . ' -> ' . $ct_qty . "\n";
     }
 
     // 재고를 이미 사용했다면 (재고에서 이미 뺐다면)
     $stock_use = $ct['ct_stock_use'];
-    if ($ct['ct_stock_use'])
-    {
-        if ($ct_status == '주문' || $ct_status == '취소' || $ct_status == '반품' || $ct_status == '품절')
-        {
+    if ($ct['ct_stock_use']) {
+        if ($ct_status == '주문' || $ct_status == '취소' || $ct_status == '반품' || $ct_status == '품절') {
             $stock_use = 0;
             // 재고에 다시 더한다.
-            if($ct['io_id']) {
+            if ($ct['io_id']) {
                 $sql = " update {$g5['g5_shop_item_option_table']}
                             set io_stock_qty = io_stock_qty + '{$ct['ct_qty']}'
                             where it_id = '{$ct['it_id']}'
@@ -101,15 +98,12 @@ for ($i=0; $i<$cnt; $i++)
 
             sql_query($sql);
         }
-    }
-    else
-    {
+    } else {
         // 재고 오류로 인한 수정
-        if ($ct_status == '배송' || $ct_status == '완료')
-        {
+        if ($ct_status == '배송' || $ct_status == '완료') {
             $stock_use = 1;
             // 재고에서 뺀다.
-            if($ct['io_id']) {
+            if ($ct['io_id']) {
                 $sql = " update {$g5['g5_shop_item_option_table']}
                             set io_stock_qty = io_stock_qty - '{$ct['ct_qty']}'
                             where it_id = '{$ct['it_id']}'
@@ -135,8 +129,7 @@ for ($i=0; $i<$cnt; $i++)
     $point_use = $ct['ct_point_use'];
     // 회원이면서 포인트가 0보다 크면
     // 이미 포인트를 부여했다면 뺀다.
-    if ($mb_id && $ct['ct_point'] && $ct['ct_point_use'])
-    {
+    if ($mb_id && $ct['ct_point'] && $ct['ct_point_use']) {
         $point_use = 0;
         //insert_point($mb_id, (-1) * ($ct[ct_point] * $ct[ct_qty]), "주문번호 $od_id ($ct_id) 취소");
         delete_point($mb_id, "@delivery", $mb_id, "$od_id,$ct_id");
@@ -145,7 +138,7 @@ for ($i=0; $i<$cnt; $i++)
     // 히스토리에 남김
     // 히스토리에 남길때는 작업|아이디|시간|IP|그리고 나머지 자료
     $now = G5_TIME_YMDHIS;
-    $ct_history="\n$ct_status|{$member['mb_id']}|$now|$REMOTE_ADDR";
+    $ct_history = "\n$ct_status|{$member['mb_id']}|$now|$REMOTE_ADDR";
 
     $sql = " update {$g5['g5_shop_cart_table']}
                 set ct_point_use  = '$point_use',
@@ -157,15 +150,15 @@ for ($i=0; $i<$cnt; $i++)
     sql_query($sql);
 
     // it_id를 배열에 저장
-    if($ct_status == '주문' || $ct_status == '취소' || $ct_status == '반품' || $ct_status == '품절' || $ct_status == '완료')
+    if ($ct_status == '주문' || $ct_status == '취소' || $ct_status == '반품' || $ct_status == '품절' || $ct_status == '완료')
         $arr_it_id[] = $ct['it_id'];
 }
 
 // 상품 판매수량 반영
-if(is_array($arr_it_id) && !empty($arr_it_id)) {
+if (is_array($arr_it_id) && !empty($arr_it_id)) {
     $unq_it_id = array_unique($arr_it_id);
 
-    foreach($unq_it_id as $it_id) {
+    foreach ($unq_it_id as $it_id) {
         $sql2 = " select sum(ct_qty) as sum_qty from {$g5['g5_shop_cart_table']} where it_id = '$it_id' and ct_status = '완료' ";
         $row2 = sql_fetch($sql2);
 
@@ -183,7 +176,7 @@ if (in_array($_POST['ct_status'], $status_cancel)) {
                 where od_id = '$od_id' ";
     $row = sql_fetch($sql);
 
-    if($row['od_count1'] == $row['od_count2']) {
+    if ($row['od_count1'] == $row['od_count2']) {
         $cancel_change = true;
 
         $pg_res_cd = '';
@@ -191,21 +184,21 @@ if (in_array($_POST['ct_status'], $status_cancel)) {
         $pg_cancel_log = '';
 
         // PG 신용카드 결제 취소일 때
-        if($pg_cancel == 1) {
+        if ($pg_cancel == 1) {
             $sql = " select * from {$g5['g5_shop_order_table']} where od_id = '$od_id' ";
             $od = sql_fetch($sql);
 
-            if($od['od_tno'] && ($od['od_settle_case'] == '신용카드' || $od['od_settle_case'] == '간편결제' || $od['od_settle_case'] == 'KAKAOPAY') || ($od['od_pg'] == 'inicis' && is_inicis_order_pay($od['od_settle_case']) )) {
-                switch($od['od_pg']) {
+            if ($od['od_tno'] && ($od['od_settle_case'] == '신용카드' || $od['od_settle_case'] == '간편결제' || $od['od_settle_case'] == 'KAKAOPAY') || ($od['od_pg'] == 'inicis' && is_inicis_order_pay($od['od_settle_case']))) {
+                switch ($od['od_pg']) {
                     case 'lg':
-                        include_once(G5_SHOP_PATH.'/settle_lg.inc.php');
+                        include_once(G5_SHOP_PATH . '/settle_lg.inc.php');
 
                         $LGD_TID = $od['od_tno'];
 
                         $xpay = new XPay($configPath, $CST_PLATFORM);
 
                         // Mert Key 설정
-                        $xpay->set_config_value('t'.$LGD_MID, $config['cf_lg_mert_key']);
+                        $xpay->set_config_value('t' . $LGD_MID, $config['cf_lg_mert_key']);
                         $xpay->set_config_value($LGD_MID, $config['cf_lg_mert_key']);
 
                         $xpay->Init_TX($LGD_MID);
@@ -215,7 +208,7 @@ if (in_array($_POST['ct_status'], $status_cancel)) {
 
                         if ($xpay->TX()) {
                             $res_cd = $xpay->Response_Code();
-                            if($res_cd != '0000' && $res_cd != 'AV11') {
+                            if ($res_cd != '0000' && $res_cd != 'AV11') {
                                 $pg_res_cd = $res_cd;
                                 $pg_res_msg = $xpay->Response_Msg();
                             }
@@ -225,9 +218,9 @@ if (in_array($_POST['ct_status'], $status_cancel)) {
                         }
                         break;
                     case 'inicis':
-                        include_once(G5_SHOP_PATH.'/settle_inicis.inc.php');
+                        include_once(G5_SHOP_PATH . '/settle_inicis.inc.php');
                         $cancel_msg = '쇼핑몰 운영자 승인 취소';
-                        
+
                         $args = array(
                             'paymethod' => get_type_inicis_paymethod($od['od_settle_case']),
                             'tid' => $od['od_tno'],
@@ -248,16 +241,16 @@ if (in_array($_POST['ct_status'], $status_cancel)) {
                         }
                         break;
                     case 'KAKAOPAY':
-                        include_once(G5_SHOP_PATH.'/settle_kakaopay.inc.php');
-                        $_REQUEST['TID']               = $od['od_tno'];
-                        $_REQUEST['Amt']               = $od['od_receipt_price'];
-                        $_REQUEST['CancelMsg']         = '쇼핑몰 운영자 승인 취소';
+                        include_once(G5_SHOP_PATH . '/settle_kakaopay.inc.php');
+                        $_REQUEST['TID'] = $od['od_tno'];
+                        $_REQUEST['Amt'] = $od['od_receipt_price'];
+                        $_REQUEST['CancelMsg'] = '쇼핑몰 운영자 승인 취소';
                         $_REQUEST['PartialCancelCode'] = 0;
-                        include G5_SHOP_PATH.'/kakaopay/kakaopay_cancel.php';
+                        include G5_SHOP_PATH . '/kakaopay/kakaopay_cancel.php';
                         break;
                     default:
-                        include_once(G5_SHOP_PATH.'/settle_kcp.inc.php');
-                        require_once(G5_SHOP_PATH.'/kcp/pp_ax_hub_lib.php');
+                        include_once(G5_SHOP_PATH . '/settle_kcp.inc.php');
+                        require_once(G5_SHOP_PATH . '/kcp/pp_ax_hub_lib.php');
 
                         // locale ko_KR.euc-kr 로 설정
                         setlocale(LC_CTYPE, 'ko_KR.euc-kr');
@@ -265,7 +258,7 @@ if (in_array($_POST['ct_status'], $status_cancel)) {
                         $c_PayPlus = new C_PP_CLI_T;
 
                         $c_PayPlus->mf_clear();
-                        
+
                         $ordr_idxx = $od['od_id'];
                         $tno = $od['od_tno'];
                         $tran_cd = '00200000';
@@ -273,21 +266,21 @@ if (in_array($_POST['ct_status'], $status_cancel)) {
                         $cust_ip = $_SERVER['REMOTE_ADDR'];
                         $bSucc_mod_type = "STSC";
 
-                        $c_PayPlus->mf_set_modx_data( "tno",      $tno                         );  // KCP 원거래 거래번호
-                        $c_PayPlus->mf_set_modx_data( "mod_type", $bSucc_mod_type              );  // 원거래 변경 요청 종류
-                        $c_PayPlus->mf_set_modx_data( "mod_ip",   $cust_ip                     );  // 변경 요청자 IP
-                        $c_PayPlus->mf_set_modx_data( "mod_desc", $cancel_msg );  // 변경 사유
+                        $c_PayPlus->mf_set_modx_data("tno", $tno);  // KCP 원거래 거래번호
+                        $c_PayPlus->mf_set_modx_data("mod_type", $bSucc_mod_type);  // 원거래 변경 요청 종류
+                        $c_PayPlus->mf_set_modx_data("mod_ip", $cust_ip);  // 변경 요청자 IP
+                        $c_PayPlus->mf_set_modx_data("mod_desc", $cancel_msg);  // 변경 사유
 
-                        $c_PayPlus->mf_do_tx( $tno,  $g_conf_home_dir, $g_conf_site_cd,
-                                              $g_conf_site_key,  $tran_cd,    "",
-                                              $g_conf_gw_url,  $g_conf_gw_port,  "payplus_cli_slib",
-                                              $ordr_idxx, $cust_ip, "3" ,
-                                              0, 0, $g_conf_key_dir, $g_conf_log_dir);
+                        $c_PayPlus->mf_do_tx($tno, $g_conf_home_dir, $g_conf_site_cd,
+                            $g_conf_site_key, $tran_cd, "",
+                            $g_conf_gw_url, $g_conf_gw_port, "payplus_cli_slib",
+                            $ordr_idxx, $cust_ip, "3",
+                            0, 0, $g_conf_key_dir, $g_conf_log_dir);
 
-                        $res_cd  = $c_PayPlus->m_res_cd;
+                        $res_cd = $c_PayPlus->m_res_cd;
                         $res_msg = $c_PayPlus->m_res_msg;
 
-                        if($res_cd != '0000') {
+                        if ($res_cd != '0000') {
                             $pg_res_cd = $res_cd;
                             $pg_res_msg = iconv_utf8($res_msg);
                         }
@@ -298,7 +291,7 @@ if (in_array($_POST['ct_status'], $status_cancel)) {
                 }
 
                 // PG 취소요청 성공했으면
-                if($pg_res_cd == '') {
+                if ($pg_res_cd == '') {
                     $pg_cancel_log = ' PG 신용카드 승인취소 처리';
                     $sql = " update {$g5['g5_shop_order_table']}
                                 set od_refund_price = '{$od['od_receipt_price']}'
@@ -309,14 +302,14 @@ if (in_array($_POST['ct_status'], $status_cancel)) {
         }
 
         // 관리자 주문취소 로그
-        $mod_history .= G5_TIME_YMDHIS.' '.$member['mb_id'].' 주문'.$_POST['ct_status'].' 처리'.$pg_cancel_log."\n";
+        $mod_history .= G5_TIME_YMDHIS . ' ' . $member['mb_id'] . ' 주문' . $_POST['ct_status'] . ' 처리' . $pg_cancel_log . "\n";
     }
 }
 
 // 미수금 등의 정보
 $info = get_order_info($od_id);
 
-if(!$info)
+if (!$info)
     alert('주문자료가 존재하지 않습니다.');
 
 $sql = " update {$g5['g5_shop_order_table']}
@@ -334,7 +327,7 @@ if ($mod_history) { // 주문변경 히스토리 기록
     $sql .= " , od_mod_history = CONCAT(od_mod_history,'$mod_history') ";
 }
 
-if($cancel_change) {
+if ($cancel_change) {
     $sql .= " , od_status = '취소' "; // 주문상품 모두 취소, 반품, 품절이면 주문 취소
 } else {
     if (isset($_POST['ct_status']) && in_array($_POST['ct_status'], $status_normal)) { // 정상인 주문상태만 기록
@@ -350,8 +343,8 @@ $qstr = "sort1=$sort1&amp;sort2=$sort2&amp;sel_field=$sel_field&amp;search=$sear
 $url = G5_ADMIN_URL . "/?dir=shop&amp;pid=orderform&amp;od_id=$od_id&amp;$qstr";
 
 // 신용카드 취소 때 오류가 있으면 알림
-if($pg_cancel == 1 && $pg_res_cd && $pg_res_msg) {
-    alert('오류코드 : '.$pg_res_cd.' 오류내용 : '.$pg_res_msg, $url);
+if ($pg_cancel == 1 && $pg_res_cd && $pg_res_msg) {
+    alert('오류코드 : ' . $pg_res_cd . ' 오류내용 : ' . $pg_res_msg, $url);
 } else {
     // 1.06.06
     $od = sql_fetch(" select od_receipt_point from {$g5['g5_shop_order_table']} where od_id = '$od_id' ");
